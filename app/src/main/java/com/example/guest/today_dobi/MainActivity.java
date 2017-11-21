9bd5ef61be65622164f666e01adb0b1eb83489ab
Yu Jin Seong <abc9816@ajou.ac.kr>
2017-11-22 02:05:08 +0900

    implement save in external storage

--- app/src/main/java/com/example/guest/today_dobi/MainActivity.java
+++ app/src/main/java/com/example/guest/today_dobi/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.guest.today_dobi;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.audiofx.EnvironmentalReverb;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -12,14 +14,22 @@ import android.widget.Button;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import static android.os.Build.VERSION_CODES.O;
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,47 +47,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
 
-                MediaPlayer mp = new MediaPlayer();
-                try {
-                    mp.setDataSource("/data/user/0/com.example.guest.today_dobi/files/test.mp3");
-                    mp.prepare();
-                    mp.start();
-                } catch (IOException e) {
-                    Log.v("알림 : ", "go to catch");
-                    e.printStackTrace();
-                }
-                mp.stop();
 
-
-//                Context c = v.getContext();
-
-
-//                MediaPlayer m = MediaPlayer.create(this, Path);
-//
-//                m.start();
-//
-//                m.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
-//                    @Override
-//                    public void onCompletion(MediaPlayer mp){
-//                        mp.stop();
-//                        mp.release();
-//                    }
-//                });
-            }
-        });
-
-        Button c_btn = findViewById(R.id.create_btn);
-        c_btn.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v){
-
-                new Thread(){
-
+                new Thread(new Runnable() {
                     @Override
-                    public void run(){
+                    public void run() {
                         String clientId = "W8r9m2AbNrNLoFeyneMx";//애플리케이션 클라이언트 아이디값";
                         String clientSecret = "nW9REFjRpu";//애플리케이션 클라이언트 시크릿값";
+
                         try {
                             String text = URLEncoder.encode("면접관님덜 클라스 오지구요.", "UTF-8"); // 13자
                             String apiURL = "https://openapi.naver.com/v1/voice/tts.bin";
@@ -100,12 +76,18 @@ public class MainActivity extends AppCompatActivity {
                                 int read = 0;
                                 byte[] bytes = new byte[1024];
                                 // 랜덤한 이름으로 mp3 파일 생성
-                                String filePath = getApplication().getFilesDir().getPath().toString() + "/test.mp3";
+//
+//                                File dir = new File(android.os.Environment.getExternalStorageDirectory() + "/", "Dobi");
+//                                if(!dir.exists()){
+//                                    dir.mkdirs();
+//                                }
 
-                                String tempname = "test";
-//                                File f = new File(filePath);
-                                java.io.File f = new java.io.File(filePath);
-                                Log.v("주소 : ", filePath);
+
+                                String tempname = "dobis";
+//                                File f = new File(android.os.Environment.getExternalStorageDirectory() + File.separator + "Dobi/" + tempname + ".mp3");
+//                                File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + tempname + ".mp3");
+//                                File f = new File(getApplicationContext().getFilesDir().getPath() + "/" + tempname + ".mp3");
+                                File f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "test.mp3");
                                 f.createNewFile();
                                 OutputStream outputStream = new FileOutputStream(f);
                                 while ((read =is.read(bytes)) != -1) {
@@ -115,6 +97,13 @@ public class MainActivity extends AppCompatActivity {
                                 is.close();
 
 
+//                                String Path_to_file = android.os.Environment.getExternalStorageDirectory() + File.separator + "Dobi/" + tempname + ".mp3";
+////                                String Path_to_file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + tempname + ".mp3";
+//////                                String Path_to_file = getApplicationContext().getFilesDir().getPath() + "/" + tempname + ".mp3";
+//                                MediaPlayer audioPlay = new MediaPlayer();
+//                                audioPlay.setDataSource(Path_to_file);
+//                                audioPlay.prepare();
+//                                audioPlay.start();
 
 
                             } else {  // 에러 발생
@@ -127,20 +116,44 @@ public class MainActivity extends AppCompatActivity {
                                 br.close();
                                 System.out.println(response.toString());
                             }
-//                            try {
-//                                FileInputStream fis = new FileInputStream("./UU.mp3");
-//                                Player mp3 = new Player(fis);
-//                                mp3.play();
-//                            }catch(Exception e) {
-//                                System.out.println(e);
-//                            }
+
                         } catch (Exception e) {
                             System.out.println(e);
                         }
-
                     }
+                }).start();
 
-                }.start();
+
+            }
+        });
+
+        Button c_btn = findViewById(R.id.create_btn);
+        c_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+//                String path = getApplicationContext().getFilesDir().getPath();
+//                String path = getApplication().getFilesDir().getPath();
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                Log.v("external", path);
+
+//                String Path_to_file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "dobis" + ".mp3";
+//                String Path_to_file = android.os.Environment.getExternalStorageDirectory() + File.separator + "Dobi/" + "dobis" + ".mp3";
+
+
+                MediaPlayer mp = new MediaPlayer();
+                try {
+                    mp.setDataSource(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/test.mp3");
+                    mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mp.prepare();
+                    mp.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+//                mp.release();
+
             }
         });
 
